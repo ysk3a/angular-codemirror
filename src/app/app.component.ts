96fd07a13d3f5c85d2d9adc60aa149384aa7c417
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-05-25 21:29:41 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,6 @@ import { oneDark, oneDarkTheme, oneDarkHighlightStyle } from '@codemirror/theme-
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-  title = 'component-overview';
   completions = [
     { label: 'panic', type: 'keyword' },
     { label: 'park', type: 'constant', info: 'Test completion' },
@@ -32,34 +31,46 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     let myEditorElement = this.myEditor.nativeElement;
-    let myExt: Extension = [basicSetup, java(), oneDark, wordHover, highlightSelectionMatches(), autocompletion({ override: [this.myCompletions, this.myCompletions3] })];
-    let state!: EditorState;
+    let myExt: Extension = [basicSetup, minimalSetup, java(), oneDark, wordHover, highlightSelectionMatches(), autocompletion({ override: [this.myCompletions, this.myCompletions3] })];
+    //     let state: EditorView = new EditorView();
 
-    try {
-      state = EditorState.create({
-        doc: `
-// if there is some error do this else that
-if (something() >= 100.00) {
-  let myVar = 200;
-  let myVar2 = "100";
-} else {
-// i have another comment
-  something2.something3();
-}
-`,
-        extensions: myExt,
-      });
-    } catch (e) {
-      //Please make sure install codemirror@6.0.1
-      //If your command was npm install codemirror, will installed 6.65.1(whatever)
-      //You will be here.
-      console.error(e);
-    }
+    //     try {
+    //       state = EditorState.create({
+    //         doc: `
+    // // if there is some error do this else that
+    // if (something() >= 100.00) {
+    //   let myVar = 200;
+    //   let myVar2 = "100";
+    // } else {
+    // // i have another comment
+    //   something2.something3();
+    // }
+    // `,
+    //         extensions: myExt,
+    //       });
+    //     } catch (e) {
+    //       //Please make sure install codemirror@6.0.1
+    //       //If your command was npm install codemirror, will installed 6.65.1(whatever)
+    //       //You will be here.
+    //       console.error('custom error msg', e);
+    //     }
 
-    //console.log(state);
+    //     //console.log(state);
 
     let view = new EditorView({
-      state,
+      state: EditorState.create({
+        doc: `
+    // if there is some error do this else that
+    if (something() >= 100.00) {
+    let myVar = 200;
+    let myVar2 = "100";
+    } else {
+    // i have another comment
+    something2.something3();
+    }
+    `,
+        extensions: myExt,
+      }),
       parent: myEditorElement,
     });
   }
